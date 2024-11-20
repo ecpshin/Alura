38f7ba1bd087b9f4a3a740a4a04312600d891cd3
ecpshin <francisco.shin@hotmail.com>
2024-11-20 09:30:21 -0300

    Atualizando aula

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
+
+const posts = [
+  { id: 1, title: "Primeiro Post" },
+  { id: 2, title: "Segundo Post" },
+  { id: 3, title: "Terceiro Post" },
+  { id: 4, title: "Quarto Post" },
+  { id: 5, title: "Quinto Post" },
+];
 
 app.listen(process.env.ENV_PORT, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/api", (req, res) => {
-  res
-    .status(200)
-    .json(["Hello World! Bem vindo ao servidor da Imersão Dev Alura"]);
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
 });
 
 app.get("/livro", (req, res) => {
